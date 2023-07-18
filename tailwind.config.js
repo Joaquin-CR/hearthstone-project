ee1f7fc9e2d41e1b6757120c81122a1eac2a4af0
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-07-18 11:08:37 -0600

    style: add fonts, colors and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,25 @@ module.exports = {
       colors: {
         'bgColor-Navbar': '#2c3858',
         'bgColor-mobileMenu': '#313f63',
+        'Color-MenuHover': '#fcd52d',
+        darkOpacity: 'rgba(44, 56, 88, 0.75)',
+      },
+      screens: {
+        widescreen: { raw: '(min-aspect-ratio: 3/2)' },
+        tallscreen: { raw: '(min-aspect-ratio: 13/20)' },
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      },
+      fontFamily: {
+        AclonicaR: ['Aclonica'],
       },
     },
   },
