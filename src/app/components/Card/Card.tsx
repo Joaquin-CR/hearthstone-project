bf4ae9a340dd41976bce4ebfec13a41707bd4f1a
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-07-21 13:29:19 -0600

    perf: adjust the like button into de card component

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -31,18 +31,18 @@ const Card: React.FC<ICardInfo> = ({
     <>
       <div className="mx-3 flex flex-col items-center ">
         <img className="w-1/2" src={img} alt="card" />
-        <div onClick={likeDislike}>
-          <div className="bg-ColorBorder-Card hover:bg-gradient-to-b hover:from-gold hover:via-gold_2 hover:to-gold_3 rounded-full p-1 w-full">
-            <div className="bg-bgColor-Card hover:bg-bgColor-CardDescroption rounded-full text-black hover:text-ColorGold p-1">
-              <Image
-                src={favorites ? FavoriteIcon : FavoriteBorderIcon}
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-        <div className="bg-ColorBorder-Card hover:bg-gradient-to-b hover:from-gold hover:via-gold_2 hover:to-gold_3 rounded-2xl p-1 w-full">
+        <div className="relative bg-ColorBorder-Card hover:bg-gradient-to-b hover:from-gold hover:via-gold_2 hover:to-gold_3 rounded-2xl p-1 w-full">
           <div className="bg-bgColor-Card hover:bg-bgColor-CardDescroption rounded-2xl py-3 text-black hover:text-ColorGold">
+            <div className="absolute -top-6 -right-3" onClick={likeDislike}>
+              <div className=" bg-ColorBorder-Card hover:bg-gradient-to-b hover:from-gold hover:via-gold_2 hover:to-gold_3 rounded-full p-1 w-full">
+                <div className="bg-bgColor-Card hover:bg-bgColor-CardDescroption rounded-full text-black hover:text-ColorGold p-1">
+                  <Image
+                    src={favorites ? FavoriteIcon : FavoriteBorderIcon}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
             <p className="font-AclonicaR  ">{name}</p>
             <p className="font-MonserratM mt-2">{type}</p>
             <p className="font-MonserratM mt-1">{rarity}</p>
