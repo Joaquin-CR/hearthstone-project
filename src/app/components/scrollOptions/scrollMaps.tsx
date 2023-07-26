27711ff87c0e7f0a770851096daf638970e5677e
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-07-26 16:50:29 -0600

    style: correctionts into the styleclasses and adding the btn to return to map

--- src/app/components/scrollOptions/scrollMaps.tsx
+++ src/app/components/scrollOptions/scrollMaps.tsx
@@ -13,11 +13,14 @@ export default function ScrollMaps() {
   const { onMouseDown } = useDraggableScroll(ref);
   return (
     <div
-      className="hidden w-full fixed top-0 z-10 md:flex flex-col overflow-y-scroll text-shadow-lg bg-cover bg-blue-950 bg-bgSideShop shadow-black no-scrollbar py-3 text-white md:w-1/4 h-screen"
+      // hidden
+      className="w-full fixed top-20 z-10 md:flex flex-col overflow-y-scroll text-shadow-lg bg-cover bg-blue-950 bg-bgSideShop shadow-black no-scrollbar py-3 text-white md:w-1/4 h-screen"
       ref={ref}
       onMouseDown={onMouseDown}
     >
-      Hola
+      <div className="">
+        <button>Test</button>
+      </div>
     </div>
   );
 }
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,11 +1,19 @@
+import Image from 'next/image';
+import LeftArrow from '../../../public/images//leftArrowGold.svg';
 import GoogleMaps from '../components/GoogleMaps/GoogleMaps';
 import ScrollMaps from '../components/scrollOptions/scrollMaps';
 
 function page() {
   return (
     <>
-      <div className="w-full overflow-y-hidden">
-        <h1 className="hidden text-xl md:block absolute font-AclonicaR z-20 top-32 left-16 md:text-7xl text-white drop-shadow-lg">
+      <div className="w-full overflow-y-hidden flex flex-col">
+        {/* hidden */}
+        <h1 className="text-5xl mt-6 flex md:block absolute font-AclonicaR z-20 md:top-32 md:left-16 md:text-7xl text-white drop-shadow-lg">
+          <div className="mx-10 md:hidden">
+            <button className="">
+              <Image src={LeftArrow} alt={''} />
+            </button>
+          </div>
           SHOPS
         </h1>
         <GoogleMaps></GoogleMaps>
