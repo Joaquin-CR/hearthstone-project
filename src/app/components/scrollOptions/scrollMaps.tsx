2ec33d7706abd921a1ea1cbe21f8fedd0417bc4e
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-07-27 12:57:56 -0600

    feat: sidemenushops

--- src/app/components/scrollOptions/scrollMaps.tsx
+++ src/app/components/scrollOptions/scrollMaps.tsx
@@ -1,6 +1,10 @@
 'use client';
-import { useRef } from 'react';
+import Image from 'next/image';
+import { useRef, useState } from 'react';
 import useDraggableScroll from 'use-draggable-scroll';
+import LeftArrow from '../../../../public/images/leftArrowGold.svg';
+import ShopCard from '../ShopCard/ShopCard';
+import ShopDetail from '../ShopCard/ShopDetail';
 
 type MapsScrollProps = {
   list?: Array<string>;
@@ -10,17 +14,130 @@ type MapsScrollProps = {
 export default function ScrollMaps() {
   const ref = useRef(null);
 
+  const [showDetail, setShowDetail] = useState(false);
+
+  const goToDetail = (card: any) => {
+    console.log('Go To Detail', card);
+    setShowDetail(!showDetail);
+  };
+
+  const goToList = () => {
+    if (showDetail) {
+      setShowDetail(!showDetail);
+    } else {
+      console.log('Close the sideMenuShops');
+    }
+  };
+
   const { onMouseDown } = useDraggableScroll(ref);
   return (
-    <div
-      // hidden
+    <div      
       className="w-full fixed top-20 z-10 md:flex flex-col overflow-y-scroll text-shadow-lg bg-cover bg-blue-950 bg-bgSideShop shadow-black no-scrollbar py-3 text-white md:w-1/4 h-screen"
       ref={ref}
       onMouseDown={onMouseDown}
     >
-      <div className="">
-        <button>Test</button>
+      <div className="flex items-center mt-12 md:justify-center">
+        <Image
+          className="md:hidden mx-9"
+          src={LeftArrow}
+          alt={''}
+          onClick={goToList}
+        />
+        <h1 className="text-5xl flex font-AclonicaR z-10 md:text-7xl text-white drop-shadow-lg">
+          SHOPS
+        </h1>
       </div>
+      {showDetail ? (
+        <div className="mt-12 mx-9">
+          <ShopDetail
+            name={'Anvil Cards'}
+            address={'2356 Bisonnet St.'}
+            open={'Open'}
+            schedule={''}
+            phone={'(832) 371-7488'}
+            net={'gemmintcardhouse.com'}
+            clickBack={() => {
+              setShowDetail(!showDetail);
+            }}
+          />
+        </div>
+      ) : (
+        <div className="mt-12 mx-9">
+          {/* ISERT A LOOP TO POPULATE THE SIDE MENU */}
+          <div className="">
+            <ShopCard
+              name={'Anvil Cards'}
+              address={'2356 Bisonnet St.'}
+              open={'Closed'}
+              schedule={'Opens 12pm'}
+              phone={'(832) 371-7488'}
+              clickCard={(card: any) => {
+                goToDetail(card);
+              }}
+            />
+          </div>
+          <div>
+            <ShopCard
+              name={'Anvil Cards'}
+              address={'2356 Bisonnet St.'}
+              open={'Open'}
+              schedule={'Close 6pm'}
+              phone={'(832) 371-7488'}
+              clickCard={function (card: any): void {
+                goToDetail(card);
+              }}
+            />
+          </div>
+          <div className="">
+            <ShopCard
+              name={'Anvil Cards'}
+              address={'2356 Bisonnet St.'}
+              open={'Closed'}
+              schedule={'Opens 12pm'}
+              phone={'(832) 371-7488'}
+              clickCard={function (card: any): void {
+                goToDetail(card);
+              }}
+            />
+          </div>
+          <div>
+            <ShopCard
+              name={'Anvil Cards'}
+              address={'2356 Bisonnet St.'}
+              open={'Open'}
+              schedule={'Close 6pm'}
+              phone={'(832) 371-7488'}
+              clickCard={function (card: any): void {
+                goToDetail(card);
+              }}
+            />
+          </div>
+          <div className="">
+            <ShopCard
+              name={'Anvil Cards'}
+              address={'2356 Bisonnet St.'}
+              open={'Closed'}
+              schedule={'Opens 12pm'}
+              phone={'(832) 371-7488'}
+              clickCard={function (card: any): void {
+                goToDetail(card);
+              }}
+            />
+          </div>
+          <div>
+            <ShopCard
+              name={'Anvil Cards'}
+              address={'2356 Bisonnet St.'}
+              open={'Open'}
+              schedule={'Close 6pm'}
+              phone={'(832) 371-7488'}
+              clickCard={function (card: any): void {
+                goToDetail(card);
+              }}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-import LeftArrow from '../../../public/images//leftArrowGold.svg';
 import GoogleMaps from '../components/GoogleMaps/GoogleMaps';
 import ScrollMaps from '../components/scrollOptions/scrollMaps';
 
@@ -7,16 +5,11 @@ function page() {
   return (
     <>
       <div className="w-full overflow-y-hidden flex flex-col">
-        {/* hidden */}
-        <h1 className="text-5xl mt-6 flex md:block absolute font-AclonicaR z-20 md:top-32 md:left-16 md:text-7xl text-white drop-shadow-lg">
-          <div className="mx-10 md:hidden">
-            <button className="">
-              <Image src={LeftArrow} alt={''} />
-            </button>
-          </div>
+        <h1 className="text-5xl mt-6 flex left-1/4 md:block absolute font-AclonicaR z-10 md:top-32 md:left-16 md:text-7xl text-white drop-shadow-lg">
           SHOPS
         </h1>
         <GoogleMaps></GoogleMaps>
+        {/* hidden */}
         <div className="">
           <ScrollMaps />
         </div>
