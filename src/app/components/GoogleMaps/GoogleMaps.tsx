d7259f3339be4d88ca701ce2c2b29ac03fb6a5ac
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-08-01 13:55:40 -0600

    feat: showing markers

--- src/app/components/GoogleMaps/GoogleMaps.tsx
+++ src/app/components/GoogleMaps/GoogleMaps.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { PlaceClass } from '../../../../types';
 
 const containerStyle = {
   width: '100%',
@@ -8,116 +9,89 @@ const containerStyle = {
 };
 
 type GoogleMapsProps = {
+  places: PlaceClass[];
   clicknMarker: (show: boolean) => void;
 };
 
-export default function GoogleMaps({ clicknMarker }: GoogleMapsProps) {
+export default function GoogleMaps({ places, clicknMarker }: GoogleMapsProps) {
+  const libraries = useMemo(() => ['places'], []);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [isMarkerHovered, setIsMarkerHovered] = useState(false);
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: '',
+    libraries: libraries as any,
+  });
+  let center = { lat: 26.3389184, lng: -98.2122496 };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }, []);
+
   const openCloseSlide = () => {
     clicknMarker(true);
   };
-
-  const [userPosition, setUserPosition] = useState<{
-    lat: number;
-    lng: number;
-  } | null>(null);
-  const [nearbyCardShops, setNearbyCardShops] = useState<
-    { lat: number; lng: number }[]
-  >([]);
-  const center = userPosition || { lat: 25.690559, lng: -100.312982 };
 
   const handleZoomChanged = () => {
     console.log('Zoom');
     // clicknMarker(true);
   };
 
-  useEffect(() => {
-    // Fetch the user's current location using the browser's geolocation API
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setUserPosition({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        },
-        (error) => {
-          console.error('Error getting user location:', error);
-        }
-      );
-    }
-  }, []);
-
-  useEffect(() => {
-    console.log('Location init', userPosition);
-    // Fetch nearby card shops using Google Maps Places API when userPosition changes
-    if (userPosition) {
-      const key = process.env.MAPS_KEY;
-      const placesAPIKey = '';
-      const radius = 1000; // Define the search radius (in meters) around the user's location
-
-      const request = {
-        location: new window.google.maps.LatLng(
-          userPosition.lat,
-          userPosition.lng
-        ),
-        radius: radius,
-        type: 'store', // Adjust the type according to the card shops category in the Places API
-        keyword: 'cards', // You can use other keywords specific to card shops
-        key: placesAPIKey,
-      };
-
-      const service = new window.google.maps.places.PlacesService(
-        document.createElement('div')
-      );
-
-      service.nearbySearch(request, (results, status) => {
-        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-          const cardShops =
-            results?.map((result) => ({
-              lat: result.geometry?.location?.lat() || 0,
-              lng: result.geometry?.location?.lng() || 0,
-            })) || [];
-
-          setNearbyCardShops(cardShops);
-        }
-      });
-    }
-    console.log('Location finish', userPosition);
-  }, [userPosition]);
-  // const key = process.env.MAPS_KEY;
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: '',
-  });
-
   return isLoaded ? (
     <>
       <div className="overflow-y-none">
         <GoogleMap
           mapContainerStyle={containerStyle}
-          center={center}
+          center={{ lat: latitude, lng: longitude }}
           zoom={10}
           onZoomChanged={handleZoomChanged}
           onClick={() => {
             clicknMarker(false);
           }}
         >
-          {userPosition && (
-            <Marker
-              position={{ lat: userPosition.lat, lng: userPosition.lng }}
-              onClick={openCloseSlide}
-              icon={{ url: '/images/MarkerMaps.png' }}
-            />
-          )}
-          {nearbyCardShops.map((shop, index) => (
-            <Marker key={index} position={{ lat: shop.lat, lng: shop.lng }} />
-          ))}
           {/* Puedes agregar marcadores u otros elementos aquí */}
+          {/* TEST MARKER */}
           <Marker
             position={center}
-            onClick={openCloseSlide}
+            onMouseOver={() => {
+              setIsMarkerHovered(true);
+            }}
+            onMouseOut={() => {
+              setIsMarkerHovered(false);
+            }}
             icon={{ url: '/images/MarkerMaps.png' }}
-          />
+            options={
+              isMarkerHovered
+                ? { animation: window.google.maps.Animation.BOUNCE }
+                : { animation: window.google.maps.Animation.DROP }
+            }
+            onClick={openCloseSlide}
+          ></Marker>
+          {/* END TEST MARKER */}
+          {places.map((place, index) => (
+            <div key={index} className=" hover:drop-shadow-blue">
+              <Marker
+                position={place.geometry.location}
+                onMouseOver={() => {
+                  setIsMarkerHovered(true);
+                }}
+                onMouseOut={() => {
+                  setIsMarkerHovered(false);
+                }}
+                icon={{ url: '/images/MarkerMaps.png' }}
+                options={
+                  isMarkerHovered
+                    ? { animation: window.google.maps.Animation.BOUNCE }
+                    : { animation: window.google.maps.Animation.DROP }
+                }
+                onClick={openCloseSlide}
+              ></Marker>
+            </div>
+          ))}
         </GoogleMap>
       </div>
     </>
