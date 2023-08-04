5d7c4473da95a4be26b74a87133d4ca5da403303
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-08-04 14:07:32 -0600

    feat: adding like and dislike funtionality with db conecction

--- src/app/components/Carousel/Carousel.tsx
+++ src/app/components/Carousel/Carousel.tsx
@@ -7,18 +7,52 @@ type CarouselProps = {
 
 export default function Carousel({ cardList }: CarouselProps) {
   const saveCardLike = async (card: CardClass, save: boolean) => {
-    console.log('Card', card);
+    console.log('Card to save', card);
     console.log('save', save);
     if (save) {
-      await fetch('/api/getData', {
+      const response = await fetch('http://localhost:3000/api/postData', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(card),
-      }).then((res) => console.log('respuesta', res));
+        body: JSON.stringify({
+          cardId: card.cardId,
+          cardName: card.cardName,
+          cardSet: card.cardSet ? card.cardSet : null,
+          type: card.type ? card.type : null,
+          rarity: card.rarity ? card.rarity : null,
+          attack: card.attack ? card.attack.toString() : null,
+          health: card.health ? card.health.toString() : null,
+          text: card.text ? card.text : null,
+          race: card.race ? card.race : null,
+          playerClass: card.playerClass ? card.playerClass : null,
+          img: card.img ? card.img : null,
+          mechanics: card.mechanics ? [card.mechanics.toString] : null,
+        }),
+      });
+      console.log(response.status == 500);
     } else {
-      console.log('Se eliminan del registro');
+      const response = await fetch('http://localhost:3000/api/deleteData', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          cardId: card.cardId,
+          cardName: card.cardName,
+          cardSet: card.cardSet ? card.cardSet : null,
+          type: card.type ? card.type : null,
+          rarity: card.rarity ? card.rarity : null,
+          attack: card.attack ? card.attack.toString() : null,
+          health: card.health ? card.health.toString() : null,
+          text: card.text ? card.text : null,
+          race: card.race ? card.race : null,
+          playerClass: card.playerClass ? card.playerClass : null,
+          img: card.img ? card.img : null,
+          mechanics: card.mechanics ? [card.mechanics.toString] : null,
+        }),
+      });
+      console.log(response.status == 500);
     }
   };
 
