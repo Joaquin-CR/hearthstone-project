0faacefba086f3e4c03e470429022ccd723859c6
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-08-04 09:00:38 -0600

    fix: return to the old api call

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,14 +1,15 @@
+import { getCards } from '@/lib/getClassCard';
 import Favorites from '../components/Favorites/Favorites';
 
 export default async function Page() {
-  // const cards = await getCards('Druid');
-  const cards = await fetch('/api/getData').then((res) => {
-    console.log('Es 200?', res.status);
-    if (res.status == 200) {
-      return res.json();
-    }
-  });
-  console.log('Cartas', cards);
+  const cards = await getCards('Druid');
+  // const cards = await fetch('/api/getData').then((res) => {
+  //   console.log('Es 200?', res.status);
+  //   if (res.status == 200) {
+  //     return res.json();
+  //   }
+  // });
+  // console.log('Cartas', cards);
   return (
     <main className="w-full h-full max-h-screen text-center">
       <Favorites cards={cards ? cards : []} />
